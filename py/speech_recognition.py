933e894ef8b750cdd9fb405504c5a1c5fd813912
11236022 <165005919+11236022@users.noreply.github.com>
2024-04-08 22:43:16 +0800

    Add files via upload

--- py/speech_recognition.py
+++ py/speech_recognition.py
@@ -1,28 +1,10 @@
-import speech_recognition as sr
-r = sr.Recognizer()
-WAV = sr.AudioFile('9ikn0-s53yn.wav')
-with WAV as source:
-    audio = r.record(source)
-print(r.recognize_google(audio, show_all=True))
+from pydub import AudioSegment
 
-# import speech_recognition as sr
+def convert_to_wav(input_file, output_file):
+    audio = AudioSegment.from_file(input_file, format="m4a")
+    audio.export(output_file, format="wav")
 
-# # 建立Recognizer物件
-# r = sr.Recognizer()
-
-# # 讀取音訊檔案
-# WAV = sr.AudioFile('ag7ut-ejpnc.wav')
-
-# with WAV as source:
-#     # 將音訊檔案轉換成音訊資料
-#     audio = r.record(source)
-
-# # 辨識音訊檔案中的語音內容
-# try:
-#     # 使用Google語音辨識API辨識音訊資料
-#     result = r.recognize_google(audio, language='zh-TW')
-#     print("辨識結果：", result)
-# except sr.UnknownValueError:
-#     print("無法辨識音訊")
-# except sr.RequestError as e:
-#     print("無法取得辨識服務：", e)
+if __name__ == "__main__":
+    input_file = "recorded_audio.m4a"
+    output_file = "recorded_audio.wav"
+    convert_to_wav(input_file, output_file)
