f5f09f7a6c3e3107adc402f33d24b6196e25a420
11236001 <165187377+LinYiChen01@users.noreply.github.com>
2024-04-23 23:31:30 +0800

    Mysql

---
+++ db/user.sql
@@ -0,0 +1,9 @@
+-- USE `113-NTUB`;
+-- CREATE TABLE `user` (
+--     user_id INT AUTO_INCREMENT PRIMARY KEY,
+--     username VARCHAR(5) NOT NULL,
+--     password VARCHAR(20) NOT NULL
+-- );
+-- INSERT INTO user (username, password) VALUES ('林', '000');
+SELECT * FROM `user`;
+-- DROP TABLE `table_name`;
