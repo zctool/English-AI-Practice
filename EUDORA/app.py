a60dadc0cad6858b899f50d1c939bd3a56d271e5
11236013 <165271670+11236013@users.noreply.github.com>
2024-06-06 01:59:32 +0800

    Add files via upload

--- EUDORA/app.py
+++ EUDORA/app.py
@@ -968,5 +968,5 @@ def allowed_file(filename):
 app.register_blueprint(admin_bp, url_prefix='/admin')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5002)
 
